b1f042e162e6ef776ab07c7a2b0292298e4f7d57
vikasairsharma7071 <57059631+vikasairsharma7071@users.noreply.github.com>
2022-10-16 18:47:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.floor(6 * Math.random())+1;
 
 var randomDiceImage = "dice"+randomNumber1+".png";
 
-var randomDiceSrc = "/images/"+randomDiceImage;
+var randomDiceSrc = "images/"+randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -13,7 +13,7 @@ image1.setAttribute("src",randomDiceSrc);
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomImage2 = "dice"+randomNumber2+".png";
-var randomImageSrc2 = "/images/"+randomImage2;
+var randomImageSrc2 = "images/"+randomImage2;
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImageSrc2);
